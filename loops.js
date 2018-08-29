3a9c926d16d3eccfcf47ed2f575738b55948b5ee
Angela Lu <angela.lu0919@gmail.com>
2018-08-29 02:14:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) { 
-      console.log("I am one strange loop")
+      array.push("I am one strange loop")
     }
     else {
-     console.log(`I am ${i} strange loops.`)
+     array.push(`I am ${i} strange loops.`)
     }
   }
+  return array
 }
 
